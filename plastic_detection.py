8fdb448cac992a5136a8d870a608f896d1076d81
Arc0926 <Arcanine0926@gmail.com>
2022-05-12 22:52:59 -0400

    Update plastic_detection.py
    added to plastic detection

--- plastic_detection.py
+++ plastic_detection.py
@@ -1,12 +1,21 @@
 
 import cv2
 import numpy as np
-def plastic_detection(img_file_path, threshold):
+def plastic_detection(img_file_path, tolerance):
     # Reading the image
     img = cv2.imread(img_file_path)
+
+    # convert to hsv colorspace
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # lower bound and upper bound for Green color
+    lower_bound = np.array([50, 20, 20])   
+    upper_bound = np.array([100, 255, 255])
+    # find the colors within the boundaries
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
     # Showing the output
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", mask)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-plastic_detection('C:/Users/Arcan/Documents/CubeSat/Images/doggy.jpg', 3)
+plastic_detection('/green.jpg', 3)
