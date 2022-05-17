f530a90d09c3707cbafbbd7d81d26fe16a241b7c
Arc0926 <Arcanine0926@gmail.com>
2022-05-16 22:40:17 -0400

    Update GIT_TRANSFER.py
    added relative path support

--- Scripts/GIT_TRANSFER.py
+++ Scripts/GIT_TRANSFER.py
@@ -1,17 +1,22 @@
-import git
+import os
 from git import Repo
 
-#def git_push():
-try:
+def git_push(img_file_path):
+    try:
+        absolutepath = os.path.abspath(__file__)
+        repoDirectory = os.path.dirname(os.path.dirname(absolutepath))
+        fileName = os.path.join(repoDirectory, img_file_path).replace("\\","/")
+        print(repoDirectory)
+        repo = Repo(repoDirectory) #PATH TO YOUR GITHUB REPO
+        repo.git.add(fileName) #PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
+        repo.index.commit('New Photo')
+        print('made the commit')
+        origin = repo.remote('origin')
+        print('added remote')
+        origin.push()
+        print('pushed changes')
 
-    repo = Repo("C:\CUBESAT\CubeSat") #PATH TO YOUR GITHUB REPO
-    repo.git.add("C:\CUBESAT\CubeSat\Images") #PATH TO YOUR IMAGES FOLDER WITHIN YOUR GITHUB REPO
-    repo.index.commit('New Photo')
-    print('made the commit')
-    origin = repo.remote('origin')
-    print('added remote')
-    origin.push()
-    print('pushed changes')
+    except:
+        print("Couldn't upload to git")
 
-except:
-    print('Couldn't upload to git')
+git_push("Images")
