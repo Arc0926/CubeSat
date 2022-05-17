823ed6f82b6f5b253265fa53f3112819c2237740
Arc0926 <Arcanine0926@gmail.com>
2022-05-16 22:40:32 -0400

    Update plastic_detection.py
    fixed parentDirectory bug

--- Scripts/plastic_detection.py
+++ Scripts/plastic_detection.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 def plastic_detection(img_file_path, tolerance):
     absolutepath = os.path.abspath(__file__)
-    parentDirectory = os.path.dirname(absolutepath)
+    parentDirectory = os.path.dirname(os.path.dirname(absolutepath))
     filename = os.path.join(parentDirectory, img_file_path).replace("\\","/")
     # Reading the image
     img = cv2.imread(filename)
