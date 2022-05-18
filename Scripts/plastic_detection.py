07e81981658d2cec57883d72ceb81ae6e277f501
Arc0926 <Arcanine0926@gmail.com>
2022-05-18 12:34:48 -0400

    Update plastic_detection.py
    edited variable name

--- Scripts/plastic_detection.py
+++ Scripts/plastic_detection.py
@@ -4,9 +4,9 @@ import numpy as np
 def plastic_detection(img_file_path, tolerance):
     absolutepath = os.path.abspath(__file__)
     parentDirectory = os.path.dirname(os.path.dirname(absolutepath))
-    filename = os.path.join(parentDirectory, img_file_path).replace("\\","/")
+    fileName = os.path.join(parentDirectory, img_file_path).replace("\\","/")
     # Reading the image
-    img = cv2.imread(filename)
+    img = cv2.imread(fileName)
 
     # convert to hsv colorspace
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
