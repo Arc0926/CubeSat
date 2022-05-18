17b4ed9d2f015a8142931f604b775f186294659c
Arc0926 <Arcanine0926@gmail.com>
2022-05-18 16:17:10 -0400

    Update plastic_detection.py
    added relative paths

--- Scripts/plastic_detection.py
+++ Scripts/plastic_detection.py
@@ -1,6 +1,7 @@
 import sys,os
 import cv2
 import numpy as np
+from PIL import Image, ImageEnhance
 def plastic_detection(img_file_path, tolerance):
     absolutepath = os.path.abspath(__file__)
     parentDirectory = os.path.dirname(os.path.dirname(absolutepath))
@@ -10,32 +11,27 @@ def plastic_detection(img_file_path, tolerance):
 
     # convert to hsv colorspace
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # lower bound and upper bound for Green, Blue, and Red color
-    lower_bound_g = np.array([50, 20, 20])   
-    upper_bound_g = np.array([100, 255, 255])
+    saturation = hsv[:, :, 1].mean()
+    print(saturation)
+    # lower bound and upper bound for Blue
 
-    lower_bound_r1 = np.array([0, 50, 50]) 
-    upper_bound_r1 = np.array([10, 255, 255]) 
-
-    lower_bound_r2 = np.array([170, 50, 50]) 
-    upper_bound_r2 = np.array([180, 255, 255]) 
-
-    lower_bound_b = np.array([100,50,30])  
+    lower_bound_b = np.array([100,60,70])  
     upper_bound_b = np.array([130,255,255])
     # find the colors within the boundaries
-    mask_g = cv2.inRange(hsv, lower_bound_g, upper_bound_g)
-    mask_r1 = cv2.inRange(hsv, lower_bound_r1, upper_bound_r1)
-    mask_r2 = cv2.inRange(hsv, lower_bound_r2, upper_bound_r2)
-    mask_r = mask_r1 + mask_r2
-    mask_b = cv2.inRange(hsv, lower_bound_b, upper_bound_b)
+    mask = cv2.inRange(hsv, lower_bound_b, upper_bound_b)
 
 
-    mask = mask_g + mask_r + mask_b
+    #mask = mask_g + mask_r + mask_b
+    mask = cv2.bitwise_not(mask)
 
+    mask_path = parentDirectory.replace("\\", "/") + '/Images/Masks/mask.jpg'
+    cv2.imwrite(mask_path, mask)
     # Showing the output
+    '''
     cv2.imshow("Image", mask)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    '''
 
 #plastic_detection('C:/Users/Arcan/Documents/CubeSat/Images/green.png', 3)
-plastic_detection('Images/green.png', 3)
+plastic_detection('Images/demo2.jpg', 3)
